a7699247b80c7b114e952f59c10f5426bf052141
qobitech <qobitek@gmail.com>
2020-06-18 22:53:06 +0100

    added header footer to all pages

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
-var navbtn = document.getElementById('m_nav');
-var c_name = document.getElementById('company_name');
-var nav = document.getElementById('main_nav');
-var tp = document.getElementById('tp_company_name');
+var navbtn = document.getElementsByClassName('m_nav');
+var c_name = document.getElementsByClassName('company_name');
+var nav = document.getElementsByClassName('main_nav_mb');
+var tp = document.getElementsByClassName('tp_company_name');
 
-navbtn.addEventListener('click',()=>{
-    setTimeout(()=>{
-        if(nav.clientHeight > 520){
-            c_name.style.transition = ".4s ease-out";
-            c_name.style.opacity = 0;
-            tp.style.transition = ".4s ease-out";
-            tp.style.opacity = 1;
-        }
-        if(nav.clientHeight < 95){
-            c_name.style.transition = ".4s ease-in";            
-            c_name.style.opacity = 1;
-            tp.style.transition = ".4s ease-in";            
-            tp.style.opacity = 0;
-        }
-    },500)
-})
+
+console.log(navbtn)
+
+for(let i=0; i < nav.length; i++){
+
+    navbtn[i].addEventListener('click',()=>{
+        setTimeout(()=>{
+            if(nav[i].clientHeight > 520){
+                c_name[i].style.transition = ".4s ease-out";
+                c_name[i].style.opacity = 0;
+                tp[i].style.transition = ".4s ease-out";
+                tp[i].style.opacity = 1;
+            }
+            if(nav[i].clientHeight < 95){
+                c_name[i].style.transition = ".4s ease-in";            
+                c_name[i].style.opacity = 1;
+                tp[i].style.transition = ".4s ease-in";            
+                tp[i].style.opacity = 0;
+            }
+        },500)
+    })
+
+}
