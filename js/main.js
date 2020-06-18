58ff1fb9b1bced74367c9a3726b8db29b6a7b387
qobitech <qobitek@gmail.com>
2020-06-18 19:10:46 +0100

    added effect on company name in mobile nav

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 var navbtn = document.getElementById('m_nav');
 var c_name = document.getElementById('company_name');
-var nav = document.getElementById('main_nav')
+var nav = document.getElementById('main_nav');
+var tp = document.getElementById('tp_company_name');
 
 navbtn.addEventListener('click',()=>{
     setTimeout(()=>{
         if(nav.clientHeight > 520){
             c_name.style.transition = ".4s ease-out";
             c_name.style.opacity = 0;
+            tp.style.transition = ".4s ease-out";
+            tp.style.opacity = 1;
         }
         if(nav.clientHeight < 95){
             c_name.style.transition = ".4s ease-in";            
             c_name.style.opacity = 1;
+            tp.style.transition = ".4s ease-in";            
+            tp.style.opacity = 0;
         }
     },500)
 })
