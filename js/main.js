a319259a9b883494c60964cc3ed9c9409488443d
Paul Tobiloba <48536008+Toby-Codes@users.noreply.github.com>
2020-06-20 09:46:21 +0100

    Revert "Added full details for the Zuri Landing Page"

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-var navbtn = document.getElementsByClassName('m_nav'); //Class of toggle button
-var c_name = document.getElementsByClassName('company_name'); //Zuri studio text for nav
-var nav = document.getElementsByClassName('main_nav_mb'); //Div of The mb container 
-var tp = document.getElementsByClassName('tp_company_name'); //Zuri studio text for nav
+var navbtn = document.getElementsByClassName('m_nav');
+var c_name = document.getElementsByClassName('company_name');
+var nav = document.getElementsByClassName('main_nav_mb');
+var tp = document.getElementsByClassName('tp_company_name');
 
 
-console.log(navbtn);
-console.log(nav.length);
+console.log(navbtn)
 
 for(let i=0; i < nav.length; i++){
 
